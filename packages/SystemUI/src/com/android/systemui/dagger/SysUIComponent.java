01cc938247d96f020c7a4a24e585ea901795909d
kdrag0n <danny@kdrag0n.dev>
2021-10-30 17:17:29 +0530

    SystemUI: Use colorkt-based dynamic theme engine for Monet [1/2]
    * also allow subclasses of ThemeOverlayController to override reevaluateSystemTheme
    * added protected methods to get generated colors, so as to override in kdrag0n's impl
    
    Build colorkt and themelib
    
    Use colorkt-based dynamic theme engine for Monet
    
    Dynamic wallpaper-based theming ("Monet") is a prominent aspect of
    Android 12's Material You design system. Implement the system-wide
    wallpaper-based theming using my colorkt-based [1] dynamic theme engine [2],
    which many users prefer over Google's Pixel implementation in empirical
    tests as it typically keeps colors closer to the wallpaper and doesn't
    suffer from CAM16's blue-purple hue shift issues. It also provides
    better perceived contrast than Google's implementation in most cases
    because it uses ZCAM's modern lightness correlate as compared to CIELAB
    L* [3].
    
    NB: Unlike Google's implementation, we overlay the framework's default
    modulated surface colors for performance (no need to modulate colors
    when inflating views at runtime) and consistency (reduced dependency on
    framework-side color code, which may not respect user preferences).
    
    Demo screenshots: https://twitter.com/kdrag0n/status/1445584174790832134
    
    [1] https://github.com/kdrag0n/colorkt
    [2] https://github.com/kdrag0n/android12-extensions
    [3] https://twitter.com/MyndexResearch/status/1435491919702888448
    
    [kdrag0n: ported to 12L]
    [jhonbou121]: adapted to A13
    
    Change-Id: I48cbbacc32b2a8095eb07729aca1734f162d2391
    
    Override modulated dark QS background color
    
    Similar to how we handle other modulated surface colors, override our
    new dark QS background color for performance (no need to modulate the
    color when inflating views at runtime) and consistency (reduced
    dependency on framework-side color code, which may not follow user
    preferences).
    
    Change-Id: I8e49084f31c95dfd1682ba9144ad543b9e08479e
    
    Expose theme engine parameters as settings
    
    This adds secure settings for the following theme engine parameters,
    similar to Android 12 Extensions:
      - Colorfulness (default 1.0):
        adb shell settings put secure monet_engine_chroma_factor 1.5
      - Custom lightness scale (default 0):
        adb shell settings put secure monet_engine_linear_lightness 1
      - Brightness (default 425, range 0-1000; depends on custom lightness):
        adb shell settings put secure monet_engine_white_luminance_user 200
      - Accurate shades (default 1):
        adb shell settings put secure monet_engine_accurate_shades 0
      - Color override (default null):
        adb shell settings put secure monet_engine_color_override #ff0000
        To reset: adb shell settings delete secure monet_engine_color_override
    
    To apply changes, reboot or run the following command (requires root):
      adb shell su -c am crash com.android.systemui
    
    These settings are not currently exposed through a GUI, so for now, they
    are hidden settings that advanced users can change manually.
    
    Observe monet settings and reload theme on change
    
    Override getAccent*() and getNeutral*() methods
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
@@ -70,7 +70,7 @@ import dagger.Subcomponent;
         QsFrameTranslateModule.class,
         SystemUIBinder.class,
         SystemUIModule.class,
-        SystemUICoreStartableModule.class,
+        customSystemUICoreStartableModule.class,
         ReferenceSystemUIModule.class})
 public interface SysUIComponent {
 
